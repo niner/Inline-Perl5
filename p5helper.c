edc52e2cc3405d9b1a0b5293ebf79d67675b4156
Stefan Seifert <nine@detonation.org>
2014-08-24 13:23:15 +0200

    Properly initialize XS loading in Perl 5

--- p5helper.c
+++ p5helper.c
@@ -3,12 +3,22 @@
 
 static PerlInterpreter *my_perl;
 
+static void xs_init (pTHX);
+EXTERN_C void boot_DynaLoader (pTHX_ CV* cv);
+EXTERN_C void boot_Socket (pTHX_ CV* cv);
+
+EXTERN_C void xs_init(pTHX) {
+    char *file = __FILE__;
+    /* DynaLoader is a special case */
+    newXS("DynaLoader::boot_DynaLoader", boot_DynaLoader, file);
+}
+
 PerlInterpreter *init_perl() {
     char *embedding[] = { "", "-e", "0" };
     PERL_SYS_INIT3(0, NULL, NULL);
     my_perl = perl_alloc();
     perl_construct( my_perl );
-    perl_parse(my_perl, NULL, 3, embedding, NULL);
+    perl_parse(my_perl, xs_init, 3, embedding, NULL);
     PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
     perl_run(my_perl);
     return my_perl;
