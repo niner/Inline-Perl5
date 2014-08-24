7e639f24a9b4116837b415a5fe3b69312e4994d0
Stefan Seifert <nine@detonation.org>
2014-08-24 14:07:06 +0200

    Move PerlInterpreter to Inline::Perl5 module

---
+++ Inline/Perl5.pm6
@@ -0,0 +1,35 @@
+module Inline::Perl5;
+
+use NativeCall;
+
+class PerlInterpreter is repr('CPointer') {
+    sub Perl_SvIOK(PerlInterpreter, OpaquePointer)
+        is native("%*ENV<HOME>/interop/p5helper.so")
+        returns Int { * }
+    sub Perl_SvPOK(PerlInterpreter, OpaquePointer)
+        is native("%*ENV<HOME>/interop/p5helper.so")
+        returns Int { * }
+    sub Perl_sv_iv(PerlInterpreter, OpaquePointer)
+        is native('/usr/lib/perl5/5.18.1/x86_64-linux-thread-multi/CORE/libperl.so')
+        returns Int { * }
+    sub sv_to_char_star(PerlInterpreter, OpaquePointer)
+        is native("%*ENV<HOME>/interop/p5helper.so")
+        returns Str { * }
+    sub Perl_eval_pv(PerlInterpreter, Str, Int)
+        is native('/usr/lib/perl5/5.18.1/x86_64-linux-thread-multi/CORE/libperl.so')
+        returns OpaquePointer { * }
+
+    method run($perl) {
+        my $res = Perl_eval_pv(self, $perl, 1);
+        if Perl_SvIOK(self, $res) {
+            return Perl_sv_iv(self, $res);
+        }
+        if Perl_SvPOK(self, $res) {
+            return sv_to_char_star(self, $res);
+        }
+        return $res;
+    }
+}
+
+sub init_perl() is export is native("%*ENV<HOME>/interop/p5helper.so") returns PerlInterpreter { * }
+
--- inline_perl5.pl
+++ inline_perl5.pl
@@ -1,38 +1,8 @@
 #!/home/nine/install/rakudo/install/bin/perl6
 
 use v6;
-use NativeCall;
-
-class PerlInterpreter is repr('CPointer') {
-    sub Perl_SvIOK(PerlInterpreter, OpaquePointer)
-        is native("%*ENV<HOME>/interop/p5helper.so")
-        returns Int { * }
-    sub Perl_SvPOK(PerlInterpreter, OpaquePointer)
-        is native("%*ENV<HOME>/interop/p5helper.so")
-        returns Int { * }
-    sub Perl_sv_iv(PerlInterpreter, OpaquePointer)
-        is native('/usr/lib/perl5/5.18.1/x86_64-linux-thread-multi/CORE/libperl.so')
-        returns Int { * }
-    sub sv_to_char_star(PerlInterpreter, OpaquePointer)
-        is native("%*ENV<HOME>/interop/p5helper.so")
-        returns Str { * }
-    sub Perl_eval_pv(PerlInterpreter, Str, Int)
-        is native('/usr/lib/perl5/5.18.1/x86_64-linux-thread-multi/CORE/libperl.so')
-        returns OpaquePointer { * }
-
-    method run($perl) {
-        my $res = Perl_eval_pv(self, $perl, 1);
-        if Perl_SvIOK(self, $res) {
-            return Perl_sv_iv(self, $res);
-        }
-        if Perl_SvPOK(self, $res) {
-            return sv_to_char_star(self, $res);
-        }
-        return $res;
-    }
-}
-
-sub init_perl() is native("%*ENV<HOME>/interop/p5helper.so") returns PerlInterpreter { * }
+use lib '.';
+use Inline::Perl5;
 
 my $i = init_perl();
 say $i.run('
