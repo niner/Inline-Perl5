3987226412c5b8d27024be6cfc4ec8338067672b
Stefan Seifert <nine@detonation.org>
2014-08-24 02:41:27 +0200

    polishing

--- inline_perl5.pl
+++ inline_perl5.pl
@@ -1,16 +1,20 @@
 #!/home/nine/install/rakudo/install/bin/perl6
+
 use v6;
 use NativeCall;
 
 class PerlInterpreter is repr('CPointer') {
     sub Perl_eval_pv(PerlInterpreter, Str, Int)
         is native('/usr/lib/perl5/5.18.1/x86_64-linux-thread-multi/CORE/libperl.so') { * }
+
     method run($perl) {
         Perl_eval_pv(self, $perl, 1);
     }
 }
 
-sub init_perl() is native('/home/nine/interop/p5helper.so') returns PerlInterpreter { * }
+sub init_perl() is native("%*ENV<HOME>/interop/p5helper.so") returns PerlInterpreter { * }
 
 my $i = init_perl();
 $i.run('print "hello world\n"');
+
+# vim: ft=perl6
