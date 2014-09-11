bc5610b0b976e72cbc0f990c959a84a465cfc8f4
Stefan Seifert <nine@detonation.org>
2014-09-11 21:40:43 +0200

    Remove static my_perl variable to allow true multiplicity

--- p5helper.c
+++ p5helper.c
@@ -2,8 +2,6 @@
 #include <XSUB.h>
 #include <perl.h>
 
-static PerlInterpreter *my_perl;
-
 static void xs_init (pTHX);
 EXTERN_C void boot_DynaLoader (pTHX_ CV* cv);
 EXTERN_C void boot_Socket (pTHX_ CV* cv);
@@ -20,7 +18,7 @@ EXTERN_C void xs_init(pTHX) {
 PerlInterpreter *p5_init_perl() {
     char *embedding[] = { "", "-e", "0" };
     PERL_SYS_INIT3(0, NULL, NULL);
-    my_perl = perl_alloc();
+    PerlInterpreter *my_perl = perl_alloc();
     perl_construct( my_perl );
     perl_parse(my_perl, xs_init, 3, embedding, NULL);
     PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
