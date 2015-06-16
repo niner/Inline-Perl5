a1c1d93b4dfaeeb3150a8f2f622cd7f091d0923d
Stefan Seifert <nine@detonation.org>
2015-06-16 09:06:04 +0200

    Check for presence of Filter::Simple P5 module in build scripts
    Should give the user a somewhat more usefull error message than
    just test failures.
    
    Thanks to quester++ for the suggestion!
    
    Fixes GH#25

--- Build.pm
+++ Build.pm
@@ -6,6 +6,12 @@ use LibraryMake;
 
 class Build is Panda::Builder {
     method build($dir) {
+        shell('perl -e "use v5.18;"')
+            or die "\nPerl 5 version requirement not met\n";
+
+        shell('perl -MFilter::Simple -e ""')
+            or die "\nPlease install the Filter::Simple Perl 5 module!\n";
+
         my Str $blib = "$dir/blib";
         rm_f("$dir/lib/Inline/p5helper.so");
         rm_rf($blib);
--- configure.pl6
+++ configure.pl6
@@ -3,7 +3,10 @@ use v6;
 use LibraryMake;
 
 shell('perl -e "use v5.18;"')
-    or die "Perl 5 version requirement not met";
+    or die "\nPerl 5 version requirement not met\n";
+
+shell('perl -MFilter::Simple -e ""')
+    or die "\nPlease install the Filter::Simple Perl 5 module!\n";
 
 my %vars = get-vars('.');
 process-makefile('.', %vars);
