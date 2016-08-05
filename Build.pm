07ad97b138139a00c44c4f179c0eed2b6ed08b3d
Stefan Seifert <nine@detonation.org>
2016-08-05 14:07:16 +0200

    Use platform specific path separator in Makefile

--- Build.pm
+++ Build.pm
@@ -12,7 +12,8 @@ class Build is Panda::Builder {
             or die "\nPlease install the Filter::Simple Perl 5 module!\n";
 
         my %vars = get-vars($dir);
-        %vars<p5helper> = $*VM.platform-library-name('p5helper'.IO);
+        %vars<p5helper> = %vars<DESTDIR>.IO.child('resources').child('libraries')
+            .child($*VM.platform-library-name('p5helper'.IO)).Str;
         %vars<perlopts> = run(<perl -MExtUtils::Embed -e ccopts -e ldopts>, :out).out.lines.join('');
         mkdir "$dir/resources" unless "$dir/resources".IO.e;
         mkdir "$dir/resources/libraries" unless "$dir/resources/libraries".IO.e;
--- configure.pl6
+++ configure.pl6
@@ -9,7 +9,8 @@ shell('perl -MFilter::Simple -e ""')
     or die "\nPlease install the Filter::Simple Perl 5 module!\n";
 
 my %vars = get-vars('.');
-%vars<p5helper> = $*VM.platform-library-name('p5helper'.IO);
+%vars<p5helper> = %vars<DESTDIR>.IO.child('resources').child('libraries')
+    .child($*VM.platform-library-name('p5helper'.IO)).Str;
 %vars<perlopts> = run(<perl -MExtUtils::Embed -e ccopts -e ldopts>, :out).out.lines.join('');
 mkdir "resources" unless "resources".IO.e;
 mkdir "resources/libraries" unless "resources/libraries".IO.e;
