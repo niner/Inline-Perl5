niner/Inline-Perl5

commit 81243ea6518267be718bf5e092e9c09b9a36f281

Fix segfault when creating multiple instances without MULTIPLICITY
When creating multiple Inline::Perl5 instances in the hope to run multiple
Perl 5 interpreters, we segfaulted if perl was not compiled with -DMULTIPLICITY
Fixed by recognizing the situation and throwing an
X::Inline::Perl5::NoMultiplicity exception instead.

Thanks to lizmat++ for the report!

lib/Inline/Perl5.pm6
@@ -895,9 +895,16 @@ method retrieve_scalar_context() {
 role Perl5Caller {
 }
 
+class X::Inline::Perl5::NoMultiplicity is Exception {
+    method message() {
+        "You need to compile perl with -DMULTIPLICITY for running multiple interpreters."
+    }
+}
+
 method BUILD(*%args) {
     $!external_p5 = %args<p5>:exists;
     $!p5 = $!external_p5 ?? %args<p5> !! p5_init_perl();
+    X::Inline::Perl5::NoMultiplicity.new.throw unless $!p5.defined;
 
     &!call_method = sub (Int $index, Str $name, Int $context, OpaquePointer $args, OpaquePointer $err) returns OpaquePointer {
         my $p6obj = $objects.get($index);

META.info
@@ -1,6 +1,6 @@
 {
     "name"           : "Inline::Perl5",
-    "version"       : "*",
+    "version"       : "0.1",
     "perl"          : "v6.0.0",
     "author"        : "github:niner",
     "authors"       : [ "Stefan Seifert" ],

p5helper.c
@@ -37,8 +37,15 @@ PerlInterpreter *p5_init_perl() {
     char *embedding[] = { "", "-e", "0" };
     int argc = 0;
     char **argv;
-    if (!inited++)
+    if (inited) {
+#ifndef MULTIPLICITY
+        return NULL;
+#endif
+    }
+    else {
+        inited = 1;
         PERL_SYS_INIT(&argc, &argv);
+    }
 
     interpreters++;
 

t/use.t
@@ -24,8 +24,14 @@ $p5.use('Test::More');
 $p5.use('Test::More');
 
 # Only the first interpreter should create a Perl 6 package
-my @p5;
-@p5.push: Inline::Perl5.new xx 10;
-@p5>>.use('File::Temp');
+{
+    my @p5;
+    @p5.push: Inline::Perl5.new xx 10;
+    @p5>>.use('File::Temp');
+    CATCH {
+        when X::Inline::Perl5::NoMultiplicity {
+        }
+    }
+}
 
 # vim: ft=perl6

commit de59782e4b75acb672bdb57908ff10719fa08a38

Fix bogus error in Inline::Perl6 on perl without MULTIPLICITY
Regression by commit 81243ea6518267be718bf5e092e9c09b9a36f281

Thanks to Ævar for the report!
Fixes Inline::Perl6 GH #2

lib/Inline/Perl5.pm6
@@ -939,8 +939,8 @@ method BUILD(*%args) {
     else {
         my @args = @*ARGS;
         $!p5 = p5_init_perl(@args.elems + 4, CArray[Str].new('', '-e', '0', '--', |@args));
+        X::Inline::Perl5::NoMultiplicity.new.throw unless $!p5.defined;
     }
-    X::Inline::Perl5::NoMultiplicity.new.throw unless $!p5.defined;
 
     &!call_method = sub (Int $index, Str $name, Int $context, Pointer $args, Pointer $err) returns Pointer {
         my $p6obj = $objects.get($index);