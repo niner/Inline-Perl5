79c670d42f00f2b112bc00883b76478e42348607
Stefan Seifert <nine@detonation.org>
2019-01-14 09:27:15 +0100

    Fix segfaults caused by missing PERL_SET_CONTEXT calls

--- p5helper.c
+++ p5helper.c
@@ -594,19 +594,25 @@ SV *p5_call_package_method(PerlInterpreter *my_perl, char *package, char *name, 
 }
 
 GV *p5_look_up_package_method(PerlInterpreter *my_perl, char *module, char *name) {
-    HV * const pkg = gv_stashpv(module, 0);
-    GV * const gv = Perl_gv_fetchmethod_autoload(aTHX_ pkg, name, TRUE);
-    if (gv && isGV(gv))
-        return gv;
-    return NULL;
+    PERL_SET_CONTEXT(my_perl);
+    {
+        HV * const pkg = gv_stashpvn(module, strlen(module), 0);
+        GV * const gv = Perl_gv_fetchmethod_autoload(aTHX_ pkg, name, TRUE);
+        if (gv && isGV(gv))
+            return gv;
+        return NULL;
+    }
 }
 
 GV *p5_look_up_method(PerlInterpreter *my_perl, SV *obj, char *name) {
-    HV * const pkg = SvSTASH((SV*)SvRV(obj));
-    GV * const gv = Perl_gv_fetchmethod_autoload(aTHX_ pkg, name, TRUE);
-    if (gv && isGV(gv))
-        return gv;
-    return NULL;
+    PERL_SET_CONTEXT(my_perl);
+    {
+        HV * const pkg = SvSTASH((SV*)SvRV(obj));
+        GV * const gv = Perl_gv_fetchmethod_autoload(aTHX_ pkg, name, TRUE);
+        if (gv && isGV(gv))
+            return gv;
+        return NULL;
+    }
 }
 
 char *p5_stash_name(PerlInterpreter *my_perl, SV *obj) {
